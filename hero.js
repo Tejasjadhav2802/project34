4e8ecc02a998185864ac3879c2d35b360db440b1
Tejasjadhav2802 <79096595+Tejasjadhav2802@users.noreply.github.com>
2021-10-14 19:47:29 +0530

    Add files via upload

---
+++ hero.js
@@ -0,0 +1,27 @@
+class Hero {
+     constructor(x, y, r) 
+    { var options = 
+        {
+         'friction':1.0,
+          'density':1,
+         } 
+        this.body = Bodies.circle(x, y, this.r/2, options);
+         this.r = r;
+ this.Image=loadImage("Superhero-01.png")
+           World.add(world, this.body);
+         }
+          display()
+         { 
+             var pos =this.body.position;
+             var angle = this.body.angle;
+              push();
+               translate(pos.x, pos.y);
+               rotate(angle);
+                imageMode(CENTER);
+                strokeWeight(4);
+                 stroke("black"); 
+                fill("red");
+                 image(this.image,0, 0, this.r+150, this.r);
+                  pop();
+                 }
+                 }
